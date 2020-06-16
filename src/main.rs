96fa8830b75493a4a913e9711ea3222d48431d10
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2020-06-16 23:50:09 +0900

    simplify and_then to map

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().skip(1);
-    ubongo_score_calculator::ops::run(args).and_then(|score| -> Result<(), Box<dyn Error>> {
+    ubongo_score_calculator::ops::run(args).map(|score| -> () {
         println!("SCORE: {:?}", score);
-        Ok(()) // TODO: can i avoid this?
+        ()
     })
 }
