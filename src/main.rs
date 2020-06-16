6c035d4e6c8742cfb1d707ce17c010bec0472f95
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2020-06-16 23:50:38 +0900

    remove unneeded unit return

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().skip(1);
     ubongo_score_calculator::ops::run(args).map(|score| -> () {
         println!("SCORE: {:?}", score);
-        ()
     })
 }
