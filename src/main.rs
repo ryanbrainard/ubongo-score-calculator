2adf08a29f4136056ce9eb00897f5ed2c4a7152a
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2020-06-17 00:03:42 +0900

    KISS: just use match

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
+
 use ubongo_score_calculator::ops;
 
 fn main() {
     let args = env::args().skip(1);
-    ops::run(args).map_or_else(|err| -> () {
-        println!("ERROR: {:?}", err);
-    }, |score| -> () {
-        println!("SCORE: {:?}", score);
-    })
+    match ops::run(args) {
+        Ok(score) => println!("SCORE: {:?}", score),
+        Err(err) => println!("ERROR: {:?}", err),
+    }
 }
